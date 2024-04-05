e6e86b9a3199124588bebc21967d467c4830c2be
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-04-05 19:27:00 +0300

    add edit

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,7 +1,27 @@
 import { useEffect } from 'react';
 import axios from 'axios';
+import { useState } from 'react';
 
 const CommentsList = ({getComments,comments}) => {
+  const [edit, setEdit] = useState(null);
+  const [editComment, setEditComment] = useState('');
+
+  const cancelEdit = () => {
+    setEdit(null);
+    setEditComment('');
+  }
+
+  const submitEdit = async (e, id) => {
+    e.preventDefault();
+    try {
+      await axios.put(`/comments/${id}`, { comment: editComment });
+      cancelEdit();
+      getComments();
+    } catch (err) {
+      console.error('Error updating comment:', err);
+    }
+  };
+
   useEffect(() => {
     getComments()
   }, []);
@@ -14,14 +34,32 @@ const deleteComment = async (id) => {
   }
   getComments()
 }
+
   return (
     <div className="mt-5 d-flex flex-column gap-1" id="comments-container">
-       {comments.map((comment) => (
-        <div className="border border-1 py-1 px-1 d-flex gap-4 justify-content-between align-items-center" key={comment.id}>{comment.comment}      <button className="btn btn-danger" onClick={() => deleteComment(comment.id)}>Sil</button>
+      {comments.map((comment) => (
+        <div className="border border-1 py-1 px-1 d-flex gap-4 justify-content-between align-items-center" key={comment.id}>
+          {edit === comment.id ? (
+            <form onSubmit={(e) => submitEdit(e, comment.id)}>
+              <input
+                type="text"
+                value={editComment}
+                onChange={(e) => setEditComment(e.target.value)}
+              />
+              <button type="submit">Kaydet</button>
+              <button onClick={cancelEdit}>İptal</button>
+            </form>
+          ) : (
+            <>
+              {comment.comment}
+              <button className="btn btn-success" onClick={() => submitEdit(comment)}>Düzenle</button>
+              <button className="btn btn-danger" onClick={() => deleteComment(comment.id)}>Sil</button>
+            </>
+          )}
         </div>
       ))}
     </div>
-  )
+  );
 }
 
 export default CommentsList
